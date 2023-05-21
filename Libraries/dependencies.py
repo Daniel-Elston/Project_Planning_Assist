83ea00481be306d55efb88b5d364fa979d901f12
Daniel Thomas Elston <delstonds@outlook.com>
2023-05-21 01:31:10 +0100

    Project: Gantt Chart Generator Notes: Initial push

--- Libraries/dependencies.py
+++ Libraries/dependencies.py
@@ -12,3 +12,11 @@ import scipy.stats as stats
 import time
 import datetime
 
+
+import pandas as pd
+import numpy as np
+from random import randint
+from datetime import timedelta
+
+import plotly.express as px
+import plotly.graph_objects as go
